d6c97c6b40c1b7a45e70e39487d79d71a3f1d8f1
mohsinghani <mohsinghani.777@gmail.com>
2019-01-30 13:10:48 +0500

    add confirm signup component

---
+++ src/components/Auth/ConfirmSignUp.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import { InputField } from "./../MaterialUI";
+import "./index.css";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  p05: {
+    padding: "5px"
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  btns_parent: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "5px"
+  },
+  login_btn_parent: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginBottom: "10px"
+  }
+});
+
+class ConfirmSignUp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  goto = path => {
+    this.props.history.push(path);
+  };
+
+  render() {
+    let { classes } = this.props;
+    return (
+      <Card className="signip-container">
+        <Grid container spacing={16}>
+          <Grid item md={12} sm={12} xs={12}>
+            <h1 className={`title center ${classes.p05}`}>Confirm SignUp</h1>
+
+            <Grid container>
+              <Grid item md={12} sm={12} xs={12} className={classes.p05}>
+                <InputField
+                  label={"Confirmation Code"}
+                  variant={"outlined"}
+                  id={"confirm"}
+                  fullWidth={true}
+                  type={"number"}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container className={classes.btns_parent}>
+              <Button
+                onClick={() => this.goto("/signup")}
+                color="primary"
+                className={classes.button}
+              >
+                Back
+              </Button>
+
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Confirm
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(ConfirmSignUp);
