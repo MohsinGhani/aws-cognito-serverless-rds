2be0d75b7390d60686eeb534a4cac1ff0e833890
mohsinghani <mohsinghani.777@gmail.com>
2019-02-09 12:42:13 +0500

    update get products API

--- serverless/controller/getProducts.js
+++ serverless/controller/getProducts.js
@@ -2,11 +2,24 @@ const { client } = require("./../lib/db");
 
 function getProducts(event, context, callback) {
     const getAllProductsQuery = `
-        SELECT P.* , 
+        SELECT P.*, 
             (SELECT COUNT(PL.action) as _like FROM public."Product_Like" PL WHERE PL.product_id=P.product_id AND PL.action=true),
             (SELECT COUNT(PL.action) as _dislike FROM public."Product_Like" PL WHERE PL.product_id=P.product_id AND PL.action=false),
-            (SELECT COUNT(PL.action) as _action FROM public."Product_Like" PL WHERE PL.product_id=P.product_id)
+            (SELECT COUNT(PL.action) as _action FROM public."Product_Like" PL WHERE PL.product_id=P.product_id),
+            _comments
         FROM public."Product" P
+        LEFT JOIN LATERAL (
+            SELECT
+            json_agg(
+                json_build_object(
+                    'comment_id', com.comment_id,
+                    'user_id', com.user_id,
+                    'comment', com.comment,
+                    'time', com.time
+                ) 
+            ) _comments
+            FROM public."Comment" com WHERE com.product_id=P.product_id
+        )_comments ON true
     `
     return client.query(getAllProductsQuery)
         .then((data) => {
