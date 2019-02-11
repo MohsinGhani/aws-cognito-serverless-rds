e16beadb5f7ff2589ca0dbb2361d13cc60762be0
mohsinghani <mohsinghani.777@gmail.com>
2019-02-11 11:54:52 +0500

    add response headers

--- serverless/lib/sendResponse.js
+++ serverless/lib/sendResponse.js
@@ -1,6 +1,10 @@
 const sendSuccessRes = (context, statusCode, data, message, rowCount) => {
     const result = {
         statusCode,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        },
         body: JSON.stringify({
             data,
             message,
@@ -13,6 +17,10 @@ const sendSuccessRes = (context, statusCode, data, message, rowCount) => {
 const sendErrorRes = (context, statusCode, err) => {
     const error = {
         statusCode,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        },
         body: JSON.stringify({
             error: err,
             message: err.message,
