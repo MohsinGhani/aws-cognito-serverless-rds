b509cb0f6bd2e2911f03eb29f85aac39f3bb9eef
mohsinghani <mohsinghani.777@gmail.com>
2019-02-11 11:17:37 +0500

    update get category lambda

--- serverless/controller/getCategory.js
+++ serverless/controller/getCategory.js
@@ -1,27 +1,13 @@
 const { client } = require("./../lib/db");
+const { sendSuccessRes, sendErrorRes } = require('./../lib/sendResponse')
 
 function getCategory(event, context, callback) {
     return client.query(`SELECT * FROM public."Category"`)
         .then((data) => {
-            const result = {
-                statusCode: 200,
-                body: JSON.stringify({
-                    data: data.rows,
-                    rowCount: data.rowCount,
-                }),
-            };
-            context.succeed(result)
+            sendSuccessRes(context, 200, data.rows, 'success', data.rowCount)
         })
         .catch((err) => {
-            const error = {
-                statusCode: 500,
-                body: JSON.stringify({
-                    error: err,
-                    message: err.message,
-                    stack: err.stack,
-                }),
-            };
-            context.succeed(error)
+            sendErrorRes(context, 500, err)
         })
 }
 
