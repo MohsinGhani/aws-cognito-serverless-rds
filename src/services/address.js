d3d8df605b7c94abed0d7601de1b0b6c261f244f
mohsinghani <mohsinghani.777@gmail.com>
2019-02-13 01:22:41 +0500

    add address service

---
+++ src/services/address.js
@@ -0,0 +1,21 @@
+import path from '../config/path';
+
+export class Address {
+    static getCountries() {
+        return fetch(path.countries, {
+            method: 'GET',
+        })
+    }
+    static GetState(countryCode) {
+        return fetch(path.state + countryCode, {
+            method: 'GET'
+        })
+    }
+    static GetCities(countryCode, StateCode) {
+        console.log(countryCode)
+        console.log(StateCode)
+        return fetch(path.city + countryCode + path.city1 + StateCode, {
+            mehtod: 'GET'
+        })
+    }
+}
