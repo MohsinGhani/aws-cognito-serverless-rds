86f30e5f0a179cf1f64db1ed578caa5d052d6d91
mohsinghani <mohsinghani.777@gmail.com>
2019-07-04 12:02:10 +0500

    show map as bg on signin screen

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -12,6 +12,14 @@ import SignInWithGoogle from './SignInWithGoogle'
 import SignInWithFacebook from './SignInWithFacebook'
 import { authAction } from './../../store/actions'
 import ReactLoading from 'react-loading';
+import TopNav from './../common/TopNav'
+import credentials from '../../config/credentials'
+import ReactMapboxGl from "react-mapbox-gl";
+import Location from './../common/Location'
+
+const Map = ReactMapboxGl({
+  accessToken: credentials.MAP_ACCESS_TOCKEN
+});
 
 const styles = theme => ({
   p05: {
@@ -37,7 +45,9 @@ class SignIn extends React.Component {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      latitude: 36.778259,
+      longitude: -119.417931
     }
     this.login = this.login.bind(this)
   }
@@ -72,93 +82,111 @@ class SignIn extends React.Component {
 
   render() {
     let { classes, authError, authLoader } = this.props;
+    let { latitude, longitude } = this.state
     return (
-      <Card className="signip-container">
-        <Grid container spacing={16}>
-          <Grid item md={12} sm={12} xs={12}>
-            <h1 className={`title center ${classes.p05}`}>Sign In</h1>
-            <h3 className={`sub-title center ${classes.p05}`}>
-              to continue to Productmania
-            </h3>
+      <div>
+        <Location handleLocation={(latitude, longitude) => { this.setState({ latitude, longitude }) }} />        
+        <TopNav />
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "89vh",
+            width: "100%"
+          }}
+          movingMethod={'jumpTo'}
+          center={[longitude, latitude]}
+          zoom={[12]}
+        // onClick={(map, e) => { this.props.reverseGeoCodingAction(e.lngLat) }}
+        >
+          <br />
+          <Card className="signip-container">
+            <Grid container spacing={16}>
+              <Grid item md={12} sm={12} xs={12}>
+                <h1 className={`title center ${classes.p05}`}>Sign In</h1>
+                <h3 className={`sub-title center ${classes.p05}`}>
+                  to continue to Productmania
+                </h3>
 
-            {
-              authError ? <p className="text-alert">{JSON.stringify(authError)}</p> : ''
-            }
-
-            <Grid container>
-              <Grid item md={12} sm={12} xs={12} className={classes.p05}>
-                <InputField
-                  label={"Email address"}
-                  variant={"outlined"}
-                  id={"email"}
-                  name={"email"}
-                  fullWidth={true}
-                  onChange={this.handleInput}
-                />
-              </Grid>
-            </Grid>
-
-            <Grid container>
-              <Grid item md={12} sm={12} xs={12} className={classes.p05}>
-                <InputFieldWithEndAdornment
-                  label={"Password"}
-                  variant={"outlined"}
-                  id={"password"}
-                  name={"password"}
-                  fullWidth={true}
-                  type={"password"}
-                  onChange={this.handleInput}
-                />
-              </Grid>
-            </Grid>
-
-            <Grid container className={classes.login_btn_parent}>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                fullWidth
-                onClick={this.login}
-                disabled={authLoader}
-              >
                 {
-                  authLoader ?
-                    <div>
-                      <ReactLoading type={'spin'} color={'#fff'} height={'25px'} width={'25px'} />
-                    </div> :
-                    'Login'
+                  authError ? <p className="text-alert">{JSON.stringify(authError)}</p> : ''
                 }
-              </Button>
-            </Grid>
-            <Divider />
-            <Grid container className={classes.btns_parent}>
-              <Button
-                onClick={() => this.goto("/signup")}
-                color="primary"
-                className={classes.button}
-              >
-                Create account
+
+                <Grid container>
+                  <Grid item md={12} sm={12} xs={12} className={classes.p05}>
+                    <InputField
+                      label={"Email address"}
+                      variant={"outlined"}
+                      id={"email"}
+                      name={"email"}
+                      fullWidth={true}
+                      onChange={this.handleInput}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container>
+                  <Grid item md={12} sm={12} xs={12} className={classes.p05}>
+                    <InputFieldWithEndAdornment
+                      label={"Password"}
+                      variant={"outlined"}
+                      id={"password"}
+                      name={"password"}
+                      fullWidth={true}
+                      type={"password"}
+                      onChange={this.handleInput}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container className={classes.login_btn_parent}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    fullWidth
+                    onClick={this.login}
+                    disabled={authLoader}
+                  >
+                    {
+                      authLoader ?
+                        <div>
+                          <ReactLoading type={'spin'} color={'#fff'} height={'25px'} width={'25px'} />
+                        </div> :
+                        'Login'
+                    }
+                  </Button>
+                </Grid>
+                <Divider />
+                <Grid container className={classes.btns_parent}>
+                  <Button
+                    onClick={() => this.goto("/signup")}
+                    color="primary"
+                    className={classes.button}
+                  >
+                    Create account
               </Button>
 
-              <Button
-                onClick={() => this.goto("/forgot-password")}
-                color="primary"
-                className={classes.button}
-              >
-                Forgot password?
+                  <Button
+                    onClick={() => this.goto("/forgot-password")}
+                    color="primary"
+                    className={classes.button}
+                  >
+                    Forgot password?
               </Button>
-            </Grid>
+                </Grid>
 
-            <SignInWithGoogle
-              history={this.props.history}
-            />
-            <br />
-            <SignInWithFacebook
-              history={this.props.history}
-            />
-          </Grid>
-        </Grid>
-      </Card>
+                <SignInWithGoogle
+                  history={this.props.history}
+                />
+                <br />
+                <SignInWithFacebook
+                  history={this.props.history}
+                />
+              </Grid>
+            </Grid>
+          </Card>
+        </Map>
+      </div>
     );
   }
 }
